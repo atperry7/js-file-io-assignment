87b382000e878c6301ac85e68fe993a689061fbd
Anthony Perry <atperry7@gmail.com>
2017-06-12 14:08:34 -0500

    Implemented promote and parse method, getting correct output

--- Employee.js
+++ Employee.js
@@ -1,16 +1,30 @@
 'use strict'
 
 const fs = require('fs')
+// import { readFile, writeFile } from 'fs'
+
+const buffer2json =
+  (buffer) =>
+    JSON.parse(buffer.toString())
 
 class Employee {
-  constructor (/* ??? */) {
-    // TODO
+  constructor (name, title, salary) {
+    this.name = name
+    this.title = title
+    this.salary = salary
   }
 
-  // TODO ???
-}
+  promote (title, salary) {
+    this.title = title
+    this.salary = salary
+  }
 
-// TODO ???
+  static parseFromFilePath (path) {
+    const reader = fs.readFileSync(path)
+    let emp = buffer2json(reader)
+    return new this(emp.name, emp.title, emp.salary)
+  }
+}
 
 module.exports = {
   Employee
